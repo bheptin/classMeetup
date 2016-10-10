e99eb10f9fad9b6d531de2d0932d2eb8b0f80f6e
Brian H <bheptin@gmail.com>
2016-10-10 17:10:27 -0400

    fix bugs

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Router, Route, browserHistory, IndexRoute, IndexRedirect } from 'react-router';
 import App from '../components/App';
 import Home from '../components/Home';
-import Login from '../components/Login';
+import login from '../components/login';
 import Details from '../components/Details';
 import HomeContainer from '../containers/HomeContainer';
 
@@ -11,7 +11,7 @@ const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRedirect to="/login" />
-      <Route path="login" component={Login}/>
+      <Route path="login" component={login}/>
       <Route path="home" component={HomeContainer}>
         <IndexRoute component={Home}/>
         <Route path="/home/details" component={Details}/>
